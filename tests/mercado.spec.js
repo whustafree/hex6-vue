c67c0c4749dd44459f7453e7f1b885b1462e4963
whustafree <magustoregustavo@gmail.com>
2026-02-21 10:27:31 -0300

    s1

--- tests/mercado.spec.js
+++ tests/mercado.spec.js
@@ -1,27 +1,27 @@
 import { test, expect } from '@playwright/test';
 
 test('El Mercado TCG carga correctamente', async ({ page }) => {
-  // 1. El robot entra a la sección de TCG
+  // 1. Va a la ruta de TCG
   await page.goto('http://localhost:5173/tcg');
-
-  // 2. Verifica que el título principal exista (ignoramos mayúsculas/minúsculas)
+  
+  // 2. Verifica que el título exista
   const titulo = page.locator('h2');
-  await expect(titulo).toContainText(/Mercado TCG/i);
-
-  // 3. Verifica que la barra de búsqueda esté visible
-  const buscador = page.getByPlaceholder('Buscar carta o juego...');
+  await expect(titulo).toContainText(/TCG/i);
+  
+  // 3. Verifica que la barra de búsqueda esté visible (Texto Actualizado)
+  const buscador = page.getByPlaceholder('Buscar carta, mazo o edición...');
   await expect(buscador).toBeVisible();
 });
 
 test('La Vitrina Geek carga correctamente', async ({ page }) => {
-  // 1. El robot entra a la sección de Vitrina
+  // 1. Va a la ruta de Vitrina
   await page.goto('http://localhost:5173/vitrina');
-
-  // 2. Verifica que el título principal exista
+  
+  // 2. Verifica que el título exista
   const titulo = page.locator('h2');
-  await expect(titulo).toContainText(/Vitrina Geek/i);
-
-  // 3. Verifica que la barra de búsqueda esté visible
-  const buscador = page.getByPlaceholder('Buscar figura, consola...');
+  await expect(titulo).toContainText(/Vitrina/i);
+  
+  // 3. Verifica que la barra de búsqueda esté visible (Texto Actualizado)
+  const buscador = page.getByPlaceholder('Buscar Funko, PS5, Manga...');
   await expect(buscador).toBeVisible();
 });
